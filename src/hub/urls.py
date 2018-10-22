09f5346cdf0b9353358c12e718767fb1efb75acb
Shalom Nyende <shalom.nyende@gmail.com>
2018-10-22 05:59:50 +0300

    Introduce STATIC_ROOT, adjust home URL

--- src/hub/urls.py
+++ src/hub/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', include('tools.urls'))
+    path('majormalfunctions/', admin.site.urls),
+    path('', include('tools.urls'))
 ]
